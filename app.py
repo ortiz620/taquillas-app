631116b2cf892548865f604e63923ea931b60c1a
ortiz620 <ortizsebastianp@gmail.com>
2025-07-29 19:45:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -239,5 +239,5 @@ def descargar_excel():
         download_name=filename
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
